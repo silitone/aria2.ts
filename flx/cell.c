845fb4f576043c262dc62c2aac77e09627c33a4d
silitone <120660140+silitone@users.noreply.github.com>
2021-05-01 22:54:30 +0000

    Apply CFLAGS, -Os to decompress.o to improve decompress performance during boot-up process

--- flx/cell.c
+++ flx/cell.c
@@ -4,4 +4,6 @@ static const struct {
   uint32_t width;
   uint32_t height;
   uint32_t bytes_per_pixel; /* 2:RGB16, 3:RGB, 4:RGBA */
-  ui
+  uint8_t pixel_data[8 * 8 * 4 + 1];
+} flCellPattern = {
+  8, 8
