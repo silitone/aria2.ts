5fba5353ea10e93766cb4c89692503364f9c6ffd
silitone <120660140+silitone@users.noreply.github.com>
2021-05-03 06:01:58 +0000

    0.30.0

--- flx/cell.c
+++ flx/cell.c
@@ -6,4 +6,5 @@ static const struct {
   uint32_t bytes_per_pixel; /* 2:RGB16, 3:RGB, 4:RGBA */
   uint8_t pixel_data[8 * 8 * 4 + 1];
 } flCellPattern = {
-  8, 8
+  8, 8, 4,
+  "\0|\0\377\0|\0\377\0|\0\377\0\242\0\377\0|\0\3
