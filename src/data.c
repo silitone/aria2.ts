9e9fbb4518745b167812ec076bfc07565998e47d
silitone <120660140+silitone@users.noreply.github.com>
2022-03-07 17:56:08 +0000

    More reorg

--- src/data.c
+++ src/data.c
@@ -399,4 +399,6 @@ uint16_t ngxArcDataPut(NGXARC arc, const void* data, uint32_t datalen, uint16_t 
     if (datalen > 0){
       NGXBLK next = 0;
       if (ngxBlockNextID(pos) == 0xFFFF){
-        next = ngxArcBlock(arc, arc->bl
+        next = ngxArcBlock(arc, arc->blkcnt);
+      } else {
+        next = ngxArcBlock(a
