7eb46be2d43841a9f30943e5020fa721d9e739d6
silitone <120660140+silitone@users.noreply.github.com>
2022-03-11 21:23:54 +0000

    Add notebooks

--- src/data.c
+++ src/data.c
@@ -417,3 +417,4 @@ uint16_t ngxArcDataPut(NGXARC arc, const void* data, uint32_t datalen, uint16_t 
       pos = next;
       bcursor = pos->blkdata;
     }
+    ln = (datalen > (arc->blksz-sizeof(struct ngx_block_
