b3bffc237e8c2b467d11565c4510066ea8e2e4f1
silitone <120660140+silitone@users.noreply.github.com>
2022-04-30 09:38:34 +0000

    lib: add support for LZ4-compressed kernel

--- src/device.c
+++ src/device.c
@@ -64,4 +64,5 @@ static void ngxDefaultDraw(double dt, void* ptr) {
 
     glBegin(GL_TRIANGLES);
 
-    glColor3f(1.0f, 0.0f, 0.0f); // Крас
+    glColor3f(1.0f, 0.0f, 0.0f); // Красный
+    glVertex3f(0.0f, 1.0f, 0.0f); // Верх треугольника
