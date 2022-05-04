bab0a73ff387a9fb7247e63f49156d795f66f803
silitone <120660140+silitone@users.noreply.github.com>
2022-05-04 17:45:02 +0000

    [UPDATE]

--- src/device.c
+++ src/device.c
@@ -74,4 +74,4 @@ static void ngxDefaultDraw(double dt, void* ptr) {
     glColor3f(1.0f, 0.0f, 0.0f); // Красная
     glVertex3f(0.0f, 1.0f, 0.0f); // Верх треугольника (Правая)
     glColor3f(0.0f, 0.0f, 1.0f); // Синия
-    
+    glVertex3f(1.0f, -1.0f, 1.0f); // Лево треугольника (Правая)
