f1402c873655aadb26cbf897730443461c30e1b6
silitone <120660140+silitone@users.noreply.github.com>
2022-09-19 20:09:54 +0000

    angularjs

--- src/device.c
+++ src/device.c
@@ -378,4 +378,5 @@ RENDER:
             now = ngxNow(dev);
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
             Scene(dev, now - timed);
+            glXSwapBuffers(dev->dsp.dpy, dev->dsp.win);
   
