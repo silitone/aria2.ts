b6485ca03249570aa099dd7a606552a6abe72675
silitone <120660140+silitone@users.noreply.github.com>
2022-12-07 14:09:17 +0000

    Update task.json

--- test/ngxData.c
+++ test/ngxData.c
@@ -25,3 +25,49 @@ void test000(const char* fname){
   EXPECT(ngxArcBlock(arc, 0xFFFF) == 0); // Invalid block id
 
   block = ngxArcBlock(arc, 0);
+  EXPECT(block != 0);
+
+  EXPECT(ngxBlockID(0) == 0x0); // no segfault
+  EXPECT(ngxBlockNextID(0) == 0x0); // no segfault
+
+  EXPECT(ngxBlockID(block) == 0);
+
+  nblk = ngxArcBlock(arc, 1);
+  EXPECT(nblk != 0);
+
+  EXPECT(ngxBlockID(nblk) == 1);
+  EXPECT(ngxBlockNextID(nblk) == 0xFFFF);
+
+  EXPECT(ngxBlockSetNextID(block, 1) == 0);
+  EXPECT(ngxBlockNextID(block) == 1);
+
+  EXPECT(ngxArcUpdateBlock(arc, block) == 0);
+
+  ngxBlockCleanup(&nblk);
+  ngxBlockCleanup(&block);
+  EXPECT(nblk == 0);
+  EXPECT(block == 0);
+
+  ngxArcCleanup(&arc);
+  EXPECT(arc == 0);
+}
+
+#define DLEN (300)
+
+void test001(const char* fname){
+  NGXARC arc = 0;
+  uint16_t blkid = 0xFFFF;
+  int* buf = 0;
+  uint32_t len = 0;
+  int* ibuf = malloc(sizeof(int)*DLEN);
+
+  for (int i = 0; i < DLEN; ++i){
+    ibuf[i] = rand();
+  }
+
+  arc = ngxArcInit(fname, 0);
+  blkid = ngxArcDataPut(arc, ibuf, DLEN*sizeof(int), 2);
+  EXPECT(blkid != 0xFFFF);
+
+  buf = (int*) ngxArcDataGet(arc, blkid, &len);
+  EXPECT(buf != 0);
